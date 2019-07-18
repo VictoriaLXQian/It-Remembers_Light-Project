f342da7cca0763be2bf6dfa3acbef824c7676ff7
Victoria Qian <Qian@QianmatoMBP.home>
2019-07-17 22:35:58 -0400

    LCD SCREEN HKG

---
+++ Text_HK/Text_HK.ino
@@ -0,0 +1,167 @@
+/**************************************************************************
+  This is a library for several Adafruit displays based on ST77* drivers.
+
+  Works with the Adafruit 1.8" TFT Breakout w/SD card
+    ----> http://www.adafruit.com/products/358
+  The 1.8" TFT shield
+    ----> https://www.adafruit.com/product/802
+  The 1.44" TFT breakout
+    ----> https://www.adafruit.com/product/2088
+  as well as Adafruit raw 1.8" TFT display
+    ----> http://www.adafruit.com/products/618
+
+  Check out the links above for our tutorials and wiring diagrams.
+  These displays use SPI to communicate, 4 or 5 pins are required to
+  interface (RST is optional).
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ **************************************************************************/
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+
+#ifdef ADAFRUIT_HALLOWING
+  #define TFT_CS        39 // Hallowing display control pins: chip select
+  #define TFT_RST       37 // Display reset
+  #define TFT_DC        38 // Display data/command select
+  #define TFT_BACKLIGHT  7 // Display backlight pin
+
+#elif defined(ESP32)
+  #define TFT_CS         5
+  #define TFT_RST        22 
+  #define TFT_DC         21
+  //
+  // define not needed for all pins; reference for ESP32 physical pins connections to VSPI:
+  // SDA  GPIO23 aka VSPI MOSI
+  // SCLK GPIO18 aka SCK aka VSPI SCK
+  // D/C  GPIO21 aka A0 (also I2C SDA)
+  // RST  GPIO22 aka RESET (also I2C SCL)
+  // CS   GPIO5  aka chip select
+  // LED  3.3V
+  // VCC  5V
+  // GND - GND
+  //
+#elif defined(ESP8266)
+  #define TFT_CS         4
+  #define TFT_RST        16                                            
+  #define TFT_DC         5
+
+#else
+  // For the breakout board, you can use any 2 or 3 pins.
+  // These pins will also work for the 1.8" TFT shield.
+  #define TFT_CS        10
+  #define TFT_RST        9 // Or set to -1 and connect to Arduino RESET pin
+  #define TFT_DC         8
+#endif
+
+// OPTION 1 (recommended) is to use the HARDWARE SPI pins, which are unique
+// to each board and not reassignable. For Arduino Uno: MOSI = pin 11 and
+// SCLK = pin 13. This is the fastest mode of operation and is required if
+// using the breakout board's microSD card.
+
+// For 1.44" and 1.8" TFT with ST7735 (including HalloWing) use:
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+// For 1.54" TFT with ST7789:
+//Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+// OPTION 2 lets you interface the display using ANY TWO or THREE PINS,
+// tradeoff being that performance is not as fast as hardware SPI above.
+//#define TFT_MOSI 11  // Data out
+//#define TFT_SCLK 13  // Clock out
+//Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+float p = 3.1415926;
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.print(F("Hello! ST77xx TFT Test"));
+  tft.setRotation(1);
+  // SET ROTATION IN SETUP. 0,2 - Portrait 1,3 - Landscape
+
+#ifdef ADAFRUIT_HALLOWING
+  // HalloWing is a special case. It uses a ST7735R display just like the
+  // breakout board, but the orientation and backlight control are different.
+  tft.initR(INITR_HALLOWING);        // Initialize HalloWing-oriented screen
+  pinMode(TFT_BACKLIGHT, OUTPUT);
+  digitalWrite(TFT_BACKLIGHT, HIGH); // Backlight on
+#else
+  // Use this initializer if using a 1.8" TFT screen:
+  //tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
+
+  // OR use this initializer (uncomment) if using a 1.44" TFT:
+  tft.initR(INITR_144GREENTAB); // Init ST7735R chip, green tab
+
+  // OR use this initializer (uncomment) if using a 0.96" 180x60 TFT:
+  //tft.initR(INITR_MINI160x80);  // Init ST7735S mini display
+
+  // OR use this initializer (uncomment) if using a 1.54" 240x240 TFT:
+  //tft.init(240, 240);           // Init ST7789 240x240
+#endif
+
+  Serial.println(F("Initialized"));
+
+  uint16_t time = millis();
+  tft.fillScreen(ST77XX_BLACK);
+  time = millis() - time;
+
+  Serial.println(time, DEC);
+  delay(500);
+
+  // large block of text
+  tft.fillScreen(ST77XX_BLACK);
+  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST77XX_WHITE);
+  delay(1000);
+
+  // tft print function!
+  tftPrintTest();
+  delay(4000);
+//
+//  // a single pixel
+//  tft.drawPixel(tft.width()/2, tft.height()/2, ST77XX_GREEN);
+//  delay(500);
+//
+
+  Serial.println("done");
+  delay(1000);
+}
+
+void loop() {
+//  tft.invertDisplay(true);
+//  delay(500);
+//  tft.invertDisplay(false);
+//  delay(500);
+}
+
+void testdrawtext(char *text, uint16_t color) {
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
+}
+
+void tftPrintTest() {
+  tft.setTextWrap(false);
+  tft.fillScreen(ST77XX_WHITE);
+  tft.setCursor(0, 30);
+  tft.setTextColor(ST77XX_BLACK);
+  tft.setTextSize(6);
+  tft.println("Hong Kong");
+  delay(500);
+  tft.setCursor(0, 0);
+  tft.fillScreen(ST77XX_WHITE);
+  tft.setTextColor(ST77XX_BLACK);
+  tft.setTextSize(2);
+  tft.println("It remembers");
+  tft.println("The night facing the piano");
+  tft.println("The pen on my wrist");
+  tft.println("The tears on my face");
+  delay(1500);
+}
